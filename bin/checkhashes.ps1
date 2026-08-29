cb32c3306d36423180a77ae6b8fda3b641c3bcef
syntacticallyazure <95119739+syntacticallyazure@users.noreply.github.com>
2026-08-29 11:07:25 +1000

    devops: updated checkhashes.ps1 to exclude megadot manifest

--- bin/checkhashes.ps1
+++ bin/checkhashes.ps1
@@ -1,4 +1,15 @@
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
+if (!$env:SCOOP_HOME) {
+    $env:SCOOP_HOME = Convert-Path (scoop prefix scoop)
+}
+
 $checkhashes = "$env:SCOOP_HOME/bin/checkhashes.ps1"
-$dir = "$PSScriptRoot/../bucket" # checks the parent dir
-& $checkhashes -Dir $dir @Args
+$dir = "$PSScriptRoot/../bucket"
+#megadot takes FOREVER to download and hash, with no fault to this hosting provider.
+#therefore, it is excluded from testing.
+$exclude = 'megadot'
+
+Get-ChildItem $dir -Filter '*.json' |
+    Where-Object BaseName -ne $exclude |
+    ForEach-Object {
+        & $checkhashes -Dir $dir -App $_.BaseName @Args
+    }
